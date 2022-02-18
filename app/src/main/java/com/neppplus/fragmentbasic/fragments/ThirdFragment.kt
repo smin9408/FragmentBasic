f1f30a23dbe3e6a69ec4d5b36ef789dbdf484526
smin9408 <stmdaks888@naver.com>
2022-02-18 16:08:18 +0900

    세번째 프래그먼트 완료

---
+++ app/src/main/java/com/neppplus/fragmentbasic/fragments/ThirdFragment.kt
@@ -0,0 +1,35 @@
+package com.neppplus.fragmentbasic.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.neppplus.fragmentbasic.R
+import kotlinx.android.synthetic.main.fragment_third.*
+
+class ThirdFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+        btnToast.setOnClickListener {
+
+            Toast.makeText(requireContext(), "세번째 프래그먼트", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+
+}
