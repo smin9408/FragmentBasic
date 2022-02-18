f159b96bdaf8e38d28aafd2165de1a478f208f9d
smin9408 <stmdaks888@naver.com>
2022-02-18 15:17:31 +0900

    Fragment() 상속 + xml 끌어와서 연동

--- app/src/main/java/com/neppplus/fragmentbasic/fragments/MyFirstFragment.kt
+++ app/src/main/java/com/neppplus/fragmentbasic/fragments/MyFirstFragment.kt
@@ -1,4 +1,20 @@
 package com.neppplus.fragmentbasic.fragments
 
-class MyFirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.fragmentbasic.R
+
+class MyFirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_first, container, false)
+    }
+
 }
