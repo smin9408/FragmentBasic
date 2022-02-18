a33075cccee9654a2fcc19502ef2aa401d1c2121
smin9408 <stmdaks888@naver.com>
2022-02-18 15:35:36 +0900

    프래그먼트의 동작 코드 작성  - onActivityCreate 함수 활용

--- app/src/main/java/com/neppplus/fragmentbasic/fragments/MyFirstFragment.kt
+++ app/src/main/java/com/neppplus/fragmentbasic/fragments/MyFirstFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.fragmentbasic.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.fragmentbasic.R
+import kotlinx.android.synthetic.main.fragment_my_first.*
 
 class MyFirstFragment : Fragment() {
 
@@ -17,4 +19,17 @@ class MyFirstFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_my_first, container, false)
     }
 
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//      이 프래그먼트를 들고 있는 액티비티가 완전히 만들어진 시점.
+//        이벤트 처리 / 데이터 보여주기 등등 동작.
+
+        btnLog.setOnClickListener {
+
+            Log.d("첫 프래그먼트", "프래그먼트의 버튼으로 로그 찍기")
+
+        }
+    }
 }
